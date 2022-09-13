aa9941374c58d75baea3d36220bc0513301622bf
Bruno <bruno.pacheco@tusta.com.br>
2022-09-12 21:10:56 -0300

    feat: configure to use js on build and ts on dev

--- src/backend.ts
+++ src/backend.ts
@@ -6,7 +6,9 @@ app.get('/ads', (req, res) => {
     return res.json([
         { id: 1, name: 'Anúncio 1' },
         { id: 2, name: 'Anúncio 2' },
-        { id: 3, name: 'Anúncio 3' }
+        { id: 3, name: 'Anúncio 3' },
+        { id: 4, name: 'Anúncio 4' },
+        { id: 5, name: 'Anúncio 5' }
     ])
 })
 
