a70d86974e8dfdd2d3cb4c53815acc4701c50349
Bruno <bruno.pacheco@tusta.com.br>
2022-09-20 22:13:25 -0300

    feat: create get route with param id

--- src/backend.ts
+++ src/backend.ts
@@ -2,7 +2,18 @@ import express, { response } from 'express'
 
 const app = express()
 
-app.get('/ads', (req, res) => {
+app.get('/games', (req,res) => {
+    return response.json([])
+})
+
+app.post('/ads', (req, res) => {
+    return res.json(['ads'])
+})
+
+app.get('/games/:id/ads', (req, res) => {
+
+    // const gameId = req.params.id
+
     return res.json([
         { id: 1, name: 'Anúncio 1' },
         { id: 2, name: 'Anúncio 2' },
@@ -11,5 +22,17 @@ app.get('/ads', (req, res) => {
         { id: 5, name: 'Anúncio 5' }
     ])
 })
+
+app.get('/games/:id/discord', (req, res) => {
+    
+    return res.json([
+        { id: 1, name: 'Anúncio 1' },
+        { id: 2, name: 'Anúncio 2' },
+        { id: 3, name: 'Anúncio 3' },
+        { id: 4, name: 'Anúncio 4' },
+        { id: 5, name: 'Anúncio 5' }
+    ])
+})
+
 
 app.listen(3333)
